54efc42443e838d646a21de79233ca9e53471684
gabrielevensen <64788960+gabrielevensen@users.noreply.github.com>
2021-10-12 17:31:33 +0200

    Update first_step.sh

--- first_step.sh
+++ first_step.sh
@@ -2,6 +2,10 @@
 #!/bin/sh
 
 tar -xvf murtazo.tgz
+cd murtazo
+tar -xvf cloudnaca.tgz
+tar -xvf navier_stokes_solver.tar
+cd ..
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
