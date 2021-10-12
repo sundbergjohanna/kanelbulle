694b52161e02840f9687f054ca3862733f04751d
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-10-12 12:59:08 +0200

    Update convert_xml.sh

--- convert_xml.sh
+++ convert_xml.sh
@@ -4,11 +4,15 @@
 # Usage: ./convert_to_xml.sh <directory where .msh is located>
 # eg: /home/ubuntu/murtazo/cloudnaca/msh
 
+cd cloudnaca
+#echo pwd
+cd msh
+
 for f in *.msh; do
     dolfin-convert "$f" "${f%.*}.xml"
 done
 echo "*** Moving files ***"
-for f in *.msh; do
-     rm "$f"
+for x in *.msh; do
+     rm "$x"
 done
-echo "*** Deleted msh files ***"
+echo "*** Deleting all msh ***"
