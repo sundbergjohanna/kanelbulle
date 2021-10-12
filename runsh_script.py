c59009c01893feaead50351c5200020f3327d9a9
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-10-12 15:09:37 +0200

    Update runsh_script.py

--- runsh_script.py
+++ runsh_script.py
@@ -10,16 +10,19 @@ import os
 import subprocess
 import sys
 
-script_dir = "murtazo/cloudnaca
+script_dir = "murtazo/cloudnaca"
 
 def run_script(ang_0, ang_1, n_ang, n_nodes, n_lvl):
-        
+        cwd = os.getcwd()
+        print(cwd)
         os.chdir(script_dir)
 
         try:
                 print("$ ./runme.sh", ang_0, ang_1, n_ang, n_nodes, n_lvl)
                 print("Loading...")
                 subprocess.check_call(["./runme.sh", ang_0, ang_1, n_ang, n_nodes, n_lvl])
+                os.chdir(cwd)
+                subprocess.check_call(["./convert_xml.sh"])
                 
         except subprocess.CalledProcessError:
                 print("Oops: ./runme.sh could not finish")
@@ -28,6 +31,7 @@ def run_script(ang_0, ang_1, n_ang, n_nodes, n_lvl):
                 print("Unexpected error:", sys.exc_info()[0])
                 return False
 
+
         return True
 
 
@@ -35,7 +39,6 @@ start = str(0);     stop = str(10);     nr = str(2);        nodes = str(50);    
 
 if run_script(start, stop, nr, nodes, refine_levels):
         print("*** Data generated :))) ***"
-        subprocess.check_call(["./convert_xml.sh"])
         
 else:
         print("*** Failed ://// ***")
