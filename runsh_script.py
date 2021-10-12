78e6e03804138b75ee4dc48ebfb661ba9487693a
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-10-12 14:36:16 +0200

    Update runsh_script.py

--- runsh_script.py
+++ runsh_script.py
@@ -13,7 +13,7 @@ import sys
 script_dir = "murtazo/cloudnaca"
 
 def run_script(ang_0, ang_1, n_ang, n_nodes, n_lvl):
-        cwd = os.getcwd()
+        
         os.chdir(script_dir)
 
         try:
@@ -26,13 +26,13 @@ def run_script(ang_0, ang_1, n_ang, n_nodes, n_lvl):
         except:
                 print("Unexpected error:", sys.exc_info()[0])
                 return False
-        os.chdir(cwd)
+
         return True
 
 
 start = str(0);     stop = str(10);     nr = str(2);        nodes = str(50);    refine_levels = str(1)
 
 if run_script(start, stop, nr, nodes, refine_levels):
-        print("*** Data generated ***")
+        print("*** Data generated :))) ***")
 else:
         print("*** Failed ://// ***")
