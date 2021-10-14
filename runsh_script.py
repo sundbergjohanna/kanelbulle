29623ae0e5901301025585ed7c815842d42a2a37
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-10-14 14:08:58 +0200

    Update runsh_script.py

--- runsh_script.py
+++ runsh_script.py
@@ -62,7 +62,9 @@ def retrieve_results(meshfile):
         os.chdir(airfoil_dir)
         try: 
                 os.mkdir('res_' + meshfile)
-                os.system('cd results')
+                os.system('ls -l')
+                os.chdir('results')
+                os.system('ls')
                 os.system('mv drag_ligt.m ../res_' + meshfile)
                 os.chdir(cwd)
         except:
