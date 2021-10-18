9aaa7565b152781b71a8e9ace33b9d09735db641
gabrielevensen <gabrielevensen96@gmail.com>
2021-10-18 12:02:24 +0200

    flask with html template

--- Flask/web.py
+++ Flask/web.py
@@ -1,15 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 # -------- Present result with Flask -------- #
-@app.route('/result')
-def process():
+@app.route('/simulation/<int:id>')
+def simulation(id):
     lift = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     drag = ['high', 'low', 'medium', 'small', 'high', 'low', 'medium', 'high', 'small', 'small']
     result = dict(zip(lift, drag))
-    # time.sleep(10)
-    return result
+    return render_template('test.html', sim_spec=id, results=result)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
