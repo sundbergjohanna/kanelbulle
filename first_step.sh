d3adc0434694118724f5cd6faf265fdcbaab0ba4
MartinaLundberg <66014098+MartinaLundberg@users.noreply.github.com>
2021-10-18 15:51:03 +0200

    Update first_step.sh

--- first_step.sh
+++ first_step.sh
@@ -5,8 +5,8 @@ tar -xvf murtazo.tgz
 cd murtazo
 tar -xvf cloudnaca.tgz
 tar -xvf navier_stokes_solver.tar
-cd ..
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+cd .. 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
