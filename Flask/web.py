1efcaea460c106b56b97b1bf2983376d42e2039e
gabrielevensen <gabrielevensen96@gmail.com>
2021-10-18 15:44:04 +0200

    komigen

--- Flask/web.py
+++ Flask/web.py
@@ -9,7 +9,7 @@ def simulation(id):
     lift = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     drag = ['high', 'low', 'medium', 'small', 'high', 'low', 'medium', 'high', 'small', 'small']
     result = dict(zip(lift, drag))
-    return render_template('test.html', 'style.css', sim_spec=id, results=result)
+    return render_template('test.html', sim_spec=id, results=result)
 
 
 if __name__ == '__main__':
