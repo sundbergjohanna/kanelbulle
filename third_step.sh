882a190418fb157aeb331f9565b71ef24d0c5e15
MartinaLundberg <66014098+MartinaLundberg@users.noreply.github.com>
2021-10-29 10:55:38 +0200

    Update third_step.sh

--- third_step.sh
+++ third_step.sh
@@ -1,3 +1,3 @@
-docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh this_should_work/docker_comp/murtazo_worker
-docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh this_should_work/docker_comp/flask_app
+sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh this_should_work/docker_comp/murtazo_worker
+sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh this_should_work/docker_comp/flask_app
 echo "*FINISHED"
