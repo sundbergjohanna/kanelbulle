78f1aa9d30ebbb5d2f42ade252d25205d45071f7
Isak Voltaire <83903891+ised3770@users.noreply.github.com>
2021-10-31 17:48:21 +0100

    docker-compose files for the worker VM

---
+++ docker-compose-worker/murtazo_worker/cel_mur.py
@@ -0,0 +1,68 @@
+from celery import Celery
+from pymongo import MongoClient
+import os
+import shutil
+import json
+
+app = Celery('cel_mur',
+             broker='amqp://admin:admin@192.168.2.39:5672',
+             backend='rpc://')
+
+@app.task(name='cel_mur.calculate')
+def calculate(xmlfile):
+    # ---- Run airfoil inside container ---- #
+    os.system('./navier_stokes_solver/airfoil 5 1 5. 1 '+'./' +xmlfile)
+    os.chdir("results")
+
+    with open("drag_ligt.m", "r") as file:
+        first = 0;
+        time = []
+        lift = []
+        drag = []
+        # ---- Turn result file into a dictionary ---- #
+        for line in file:
+            if not line.strip() == '':
+                if first == 0:
+                    keys = line.split('\t')
+                    #print(keys)
+                    first = 1;
+                else:
+                    values = line.split('\t')
+                    #print(values)
+                    time.append(values[0])
+                    lift.append(values[1])
+                    values[2] = values[2].strip('\n')
+                    drag.append(values[2])
+
+    res = {"mesh_file": xmlfile, keys[0]: time, keys[1]: lift, keys[2]: drag}
+    # ---- MongoDB ---- #
+    client = MongoClient('mongodb://m_admin:m_admin@192.168.2.39:27017')
+    with client:
+        db = client.db
+        db.res.insert(res)
+
+    # Remove results folder after saving output
+    os.chdir("..")
+    shutil.rmtree("results")
+
+    return "Added to database."
+
+# ------- ORIGINAL WORKING EX. ----- #
+# @app.task(name='cel_mur.calculate')
+# def calculate(xmlfile):
+#     os.system('./navier_stokes_solver/airfoil 5 1 5. 1 '+'./' +xmlfile)
+#     #os.chdir("navier_stokes_solver")
+#     #return os.listdir()
+#
+#     os.chdir("results")
+#     #file = open("drag_ligt.m", "r")
+#     #string = file.readline()
+#     with open("drag_ligt.m", "r") as file:
+#         lines = file.readlines()
+#         lines = [line.rstrip() for line in lines]
+#
+#     # Remove results folder after saving output
+#     os.chdir("..")
+#     shutil.rmtree("results")
+#
+#     return lines
