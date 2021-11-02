95924dbcd4e710037a4a0ea18f199e0874d394ad
MartinaLundberg <66014098+MartinaLundberg@users.noreply.github.com>
2021-11-02 16:52:59 +0100

    Update setup_admin.sh

--- docker-compose-master/setup_admin.sh
+++ docker-compose-master/setup_admin.sh
@@ -5,4 +5,4 @@ sudo rabbitmqctl add_vhost admin
 sudo rabbitmqctl set_permissions -p admin admin ".*" ".*" ".*"
 sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/flask_app/xmls
 sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/murtazo_worker/xmls
-sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-woker/murtazo_worker/xmls
+sudo docker cp airfoil_container:home/fenics/shared/murtazo/navier_stokes_solver /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/murtazo_worker
