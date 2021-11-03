3ae3b3b390d455f5c160042759d94622662a12cf
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-11-03 15:59:37 +0100

    Update setup.sh

--- docker-compose-worker/setup.sh
+++ docker-compose-worker/setup.sh
@@ -1,2 +1,2 @@
-sudo docker cp airfoil_container:home/fenics/shared/murtazo/navier_stokes_solver /home/ubuntu/kanelbulle/docker-compose-woker/murtazo_worker
-sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-woker/murtazo_worker/xmls
+sudo docker cp airfoil_container:home/fenics/shared/murtazo/navier_stokes_solver /home/ubuntu/kanelbulle/docker-compose-worker/murtazo_worker
+sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-worker/murtazo_worker/xmls
