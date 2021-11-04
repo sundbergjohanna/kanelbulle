80d285b4bef558677174e53b5cec2954ca4975b7
Isak Voltaire <83903891+ised3770@users.noreply.github.com>
2021-11-04 21:07:57 +0100

    added line to kill process on port 5672

--- docker-compose-master/setup_admin.sh
+++ docker-compose-master/setup_admin.sh
@@ -3,6 +3,7 @@
 sudo rabbitmqctl add_user admin admin
 sudo rabbitmqctl add_vhost admin
 sudo rabbitmqctl set_permissions -p admin admin ".*" ".*" ".*"
+sudo lsof -ti tcp:5672 | xargs sudo kill -9
 sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/flask_app/xmls
 sudo docker cp airfoil_container:home/fenics/shared/murtazo/cloudnaca/msh /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/murtazo_worker/xmls
 sudo docker cp airfoil_container:home/fenics/shared/murtazo/navier_stokes_solver /home/ubuntu/kanelbulle/docker-compose-master/docker_comp/murtazo_worker
