c47c9748f3f9d1f7ac5f178f311cbf737b7066d1
Isak Voltaire <83903891+ised3770@users.noreply.github.com>
2021-11-06 11:15:20 +0100

    checks if result already exsist in db

--- docker-compose-master/docker_comp/murtazo_worker/cel_mur.py
+++ docker-compose-master/docker_comp/murtazo_worker/cel_mur.py
@@ -1,4 +1,3 @@
-from celery import Celery
 from pymongo import MongoClient
 import os
 import shutil
@@ -10,59 +9,48 @@ app = Celery('cel_mur',
 
 @app.task(name='cel_mur.calculate')
 def calculate(xmlfile):
-    # ---- Run airfoil inside container ---- #
-    os.system('./navier_stokes_solver/airfoil 5 1 5. 1 '+'./xmls/' +xmlfile)
-    os.chdir("results")
-
-    with open("drag_ligt.m", "r") as file:
-        first = 0;
-        time = []
-        lift = []
-        drag = []
-        # ---- Turn result file into a dictionary ---- #
-        for line in file:
-            if not line.strip() == '':
-                if first == 0:
-                    keys = line.split('\t')
-                    #print(keys)
-                    first = 1;
-                else:
-                    values = line.split('\t')
-                    #print(values)
-                    time.append(values[0])
-                    lift.append(values[1])
-                    values[2] = values[2].strip('\n')
-                    drag.append(values[2])
-
-    res = {"mesh_file": xmlfile, keys[0]: time, keys[1]: lift, keys[2]: drag}
-    # ---- MongoDB ---- #
+    # MongoDB client connection
     client = MongoClient('mongodb://m_admin:m_admin@mongo:27017')
-    with client:
+    with client: # Check if result already exists
         db = client.db
-        db.res.insert(res)
+        search = db.res.find({'mesh_file': xmlfile}, {"mesh_file" : 1}).limit(1)
+        search = list(search)
+    if search:
+        return "Result already exists from file: " + xmlfile
+    else:
+        # Run airfoil on xml file
+        os.system('./navier_stokes_solver/airfoil 5 1 5. 1 '+'./xmls/' +xmlfile)
+        os.chdir("results")
 
-    # Remove results folder after saving output
-    os.chdir("..")
-    shutil.rmtree("results")
+        with open("drag_ligt.m", "r") as file:
+            first = 0;
+            time = []
+            lift = []
+            drag = []
+            # ---- Turn result file into a dictionary ---- #
+            for line in file:
+                if not line.strip() == '':
+                    if first == 0:
+                        keys = line.split('\t')
+                        #print(keys)
+                        first = 1;
+                    else:
+                        values = line.split('\t')
+                        #print(values)
+                        time.append(values[0])
+                        lift.append(values[1])
+                        values[2] = values[2].strip('\n')
+                        drag.append(values[2])
 
-    return "Added to database."
+        res = {"mesh_file": xmlfile, keys[0]: time, keys[1]: lift, keys[2]: drag}
 
-# ------- ORIGINAL WORKING EX. ----- #
-# @app.task(name='cel_mur.calculate')
-# def calculate(xmlfile):
-#     os.system('./navier_stokes_solver/airfoil 5 1 5. 1 '+'./' +xmlfile)
-#     #os.chdir("navier_stokes_solver")
-#     #return os.listdir()
-#
-#     os.chdir("results")
-#     #file = open("drag_ligt.m", "r")
-#     #string = file.readline()
-#     with open("drag_ligt.m", "r") as file:
-#         lines = file.readlines()
-#         lines = [line.rstrip() for line in lines]
-#
-#     # Remove results folder after saving output
-#     os.chdir("..")
-#     shutil.rmtree("results")
-#
-#     return lines
+        # Insert result to database
+        with client:
+            db = client.db
+            db.res.insert(res)
+
+        # Remove results folder after saving output
+        os.chdir("..")
+        shutil.rmtree("results")
+
+        return "Added to database."
